04c78ee93a5720a512f0f5c0847a8789df78708c
haoranli1031 <69249273+haoranli1031@users.noreply.github.com>
2021-12-10 18:35:35 +0000

    Deploying to gh-pages from @ PrincetonUniversity/magnet@9294f419e891cbe3daac80d5cc716301101a60e0 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g13b00e3',
+    VERSION: '0.1.dev1+g9294f41',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
